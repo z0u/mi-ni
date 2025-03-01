426fb0326071afc690f027a991e491d09e5d7b3b
Alex Fraser <alex.d.fraser@gmail.com>
2025-03-01 11:10:19 +0000

    added typing for send/receive handler

--- src/ai_eggs/comms.py
+++ src/ai_eggs/comms.py
@@ -1,22 +1,31 @@
 import asyncio
 from contextlib import asynccontextmanager
-from typing import Any, AsyncGenerator, Callable
+from typing import Any, AsyncGenerator, Callable, TypeAlias, TypeVar
 
 import modal
 
+T = TypeVar('T')
+Handler: TypeAlias = Callable[[T], None]
+
 
 @asynccontextmanager
-async def send_to(receive: Callable[[Any], None]) -> AsyncGenerator[Callable[[Any], None], None]:
+async def send_to(receive: Handler[T]) -> AsyncGenerator[Handler[T]]:
     """
     Simple communication channel between local and remote code.
+
+    Args:
+        receive: A callback function to receive messages
+
+    Yields:
+        A function that sends messages to `receive`
     """
     async with modal.Queue.ephemeral() as queue:
-        def send(value):
+        def send(value: T) -> None:
             queue.put(value)
 
         stop_event = asyncio.Event()
 
-        async def _receive():
+        async def consume() -> None:
             while not stop_event.is_set():
                 get_task = asyncio.create_task(queue.get.aio())
                 stop_task = asyncio.create_task(stop_event.wait())
@@ -27,7 +36,7 @@ async def send_to(receive: Callable[[Any], None]) -> AsyncGenerator[Callable[[An
                 if get_task in done:
                     receive(get_task.result())
 
-        task = asyncio.create_task(_receive())
+        task = asyncio.create_task(consume())
         try:
             yield send
         finally:
