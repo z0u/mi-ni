e106bc6de472394f1c85a08180fda5b3d846172a
Alex Fraser <alex.d.fraser@gmail.com>
2025-03-02 00:19:09 +0000

    ensuring all messages are taken from queue (within timeout)

--- src/ai_eggs/comms.py
+++ src/ai_eggs/comms.py
@@ -7,9 +7,8 @@ import modal
 T = TypeVar('T')
 Handler: TypeAlias = Callable[[T], None]
 
-
 @asynccontextmanager
-async def send_to(receive: Handler[T]) -> AsyncGenerator[Handler[T]]:
+async def send_to(receive: Handler[T], trailing_timout=5) -> AsyncGenerator[Handler[T]]:
     """
     Simple communication channel between local and remote code.
 
@@ -19,26 +18,42 @@ async def send_to(receive: Handler[T]) -> AsyncGenerator[Handler[T]]:
     Yields:
         A function that sends messages to `receive`
     """
-    async with modal.Queue.ephemeral() as queue:
+    async with modal.Queue.ephemeral() as q:
         def send(value: T) -> None:
-            queue.put(value)
+            q.put(('message', value))
 
         stop_event = asyncio.Event()
 
         async def consume() -> None:
+            # First phase: consume while producer is running
             while not stop_event.is_set():
-                get_task = asyncio.create_task(queue.get.aio())
+                get_task = asyncio.create_task(q.get.aio())
                 stop_task = asyncio.create_task(stop_event.wait())
                 done, _ = await asyncio.wait(
                     [get_task, stop_task],
                     return_when=asyncio.FIRST_COMPLETED,
                 )
                 if get_task in done:
-                    receive(get_task.result())
+                    key, value = get_task.result()
+                    if key == 'message':
+                        receive(value)
+
+            # Second phase: consume remaining messages with timeout
+            try:
+                async with asyncio.timeout(trailing_timout):
+                    while True:
+                        key, value = await q.get.aio()
+                        if key == 'message':
+                            receive(value)
+                        elif key == 'sentinel':
+                            break
+            except TimeoutError:
+                pass
 
         task = asyncio.create_task(consume())
         try:
             yield send
         finally:
+            q.put(('sentinel', None))
             stop_event.set()
             await task
